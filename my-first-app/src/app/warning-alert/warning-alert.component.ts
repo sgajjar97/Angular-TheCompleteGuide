c2e8f8bd8e7f37325228b8707332657463250e35
Shail Gajjar <sgajjar97@gmail.com>
2019-03-20 14:41:39 -0700

    Add files via upload

---
+++ my-first-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning-alert',
+    template: '<p>This is a warning!!!</p>',
+    styles: ['p {padding: 20px; background-color: red; border: 1px solid red;}']
+})
+export class WarningAlertComponent{
+
+}
