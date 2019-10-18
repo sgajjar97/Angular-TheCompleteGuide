fc0abf2a9bae4cdf4414b098a22d686db0b62e36
Shail Gajjar <sgajjar97@gmail.com>
2019-10-18 14:08:56 -0700

    Add files via upload

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,6 +1,11 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
+
 export class RecipeService{
+
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('#1 test recipe', 'This is a test', 'https://images.media-allrecipes.com/images/56589.png'),
         new Recipe('#2 test recipe', 'This is a test', 'https://images.media-allrecipes.com/images/56589.png')
