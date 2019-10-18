4f4a59b28e704654b2548e1f2294bace6e8af541
Shail Gajjar <sgajjar97@gmail.com>
2019-10-18 15:28:40 -0700

    Add files via upload

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,17 +1,42 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
-
+@Injectable()
 export class RecipeService{
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('#1 test recipe', 'This is a test', 'https://images.media-allrecipes.com/images/56589.png'),
-        new Recipe('#2 test recipe', 'This is a test', 'https://images.media-allrecipes.com/images/56589.png')
+        new Recipe(
+            'Classic Lasagna Recipe', 
+            'Do not overcook your noodles!', 
+            'https://hips.hearstapps.com/vidthumb/images/180820-bookazine-delish-01280-1536610916.jpg?crop=1xw:1xh;center,top&resize=768:*', 
+            [
+                new Ingredient('lasagna noodles', 1),
+                new Ingredient('sliced mozzarella', 2)
+            ]),
+        new Recipe(
+            'Easy Crêpes', 
+            'Crepes are actually SO simple to make!', 
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-basic-crepes-horizontal-1545245797.jpg?crop=0.6668xw:1xh;center,top&resize=980:*', 
+            [
+                new Ingredient('whole milk', 1),
+                new Ingredient('large eggs', 2)
+            ])
       ];
+
+
+    constructor(private slService: ShoppingListService) { }
 
     getRecipes(){
         return this.recipes.slice();
     }
+
+    addIngredientsToShoppingList(ingredient: Ingredient[]){
+        this.slService.addIngredients(ingredient);
+    }
+
+
 }
